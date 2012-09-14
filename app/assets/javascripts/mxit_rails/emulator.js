fb647efcb5742b7d6a96edf54431f8f5bced8518
Linsen Loots <linsen.loots@gmail.com>
2012-09-14 16:00:07 +0200

    Added keyboard controls to select links

--- app/assets/javascripts/mxit_rails/emulator.js
+++ app/assets/javascripts/mxit_rails/emulator.js
@@ -2,7 +2,28 @@
 //= require mxit_rails/jquery.cookie
 
 Emulator = (function() {
+  var keys = {
+    BACKSPACE: 8,
+    ENTER: 13,
+    ESCAPE: 27,
+
+    SPACE: 32,
+
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+
+    ZERO: 48,
+    NINE: 57,
+
+    A: 65,
+    Z: 90
+  };
+
   return {
+    activeLink: 0,
+
     setCookie: function(mxitId, msisdn) {
       // Create cookies.  Use only lowercase cookie names - the server expects this (case insensitivity seems dodgy)
       $.cookie('x-mxit-login', mxitId, {path: '/'});
@@ -82,15 +103,61 @@ Emulator = (function() {
     },
 
     updateIframe: function() {
-      if (Emulator.iframeElement('form').length > 0) {
+      Emulator.iframeElement('body').on('keydown', $.proxy(Emulator, 'key'));
+
+      if (this.hasInput()) {
+        $('#phone-input').attr('disabled', false);
+        this.activeLink = this.numLinks();    
+      } else {
+        $('#phone-input').attr('disabled', 'disabled').blur();
+        this.activeLink = 0;
+      }
+      this.focusLink();
+    },
+
+    key: function(e) {
+      if ((e.keyCode == keys.UP) || (e.keyCode == keys.DOWN)) {
+        if (e.keyCode == keys.UP) {
+          this.activeLink--;
+        } else {
+          this.activeLink++
+        }
+        this.focusLink();
+      }
+    },
+
+    hasInput: function() {
+      return Emulator.iframeElement('form').length > 0;
+    },
+
+    numLinks: function() {
+      return Emulator.iframeElement('a').length;
+    },
+
+    focusLink: function() {
+      var num = this.numLinks();
+      var max = this.hasInput() ? num : num - 1;
+
+      if (this.activeLink < 0) this.activeLink = max;
+      if (this.activeLink > max) this.activeLink = 0;
+
+      if (this.activeLink == num) {
+        this.focusInput();
+      } else {
+        $(Emulator.iframeElement('a')[this.activeLink]).focus();
+      }
+    },
+
+    focusInput: function() {
+      if (this.hasInput()) {
         $('#phone-input').attr('disabled', false).focus();
       } else {
         $('#phone-input').attr('disabled', 'disabled').blur();
-      }  
+      }
     },
 
     submit: function(e) {
-      if (e.charCode == 13) {
+      if (e.charCode == keys.ENTER) {
         if (Emulator.iframeElement('form').length > 0) {
           Emulator.iframeElement('input[type=text]').val($('#phone-input').val());
           Emulator.iframeElement('input[type=submit]').click();
@@ -115,6 +182,7 @@ $(function() {
     Emulator.clearCredentials();
   }
 
+  $('body').on('keydown', $.proxy(Emulator, 'key'));
   $('#phone-input').on('keypress', $.proxy(Emulator, 'submit'))
 
 });
