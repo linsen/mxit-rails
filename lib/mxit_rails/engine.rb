a3393a88984a90617b9b559777a579206f76a293
Linsen Loots <linsen.loots@gmail.com>
2012-09-18 12:34:18 +0200

    Trying to get asset precompilation working;  Upped version to 0.1.4

--- lib/mxit_rails/engine.rb
+++ lib/mxit_rails/engine.rb
@@ -1,4 +1,7 @@
 module MxitRails
   class Engine < ::Rails::Engine
+    initializer "precompile", :group => :all do |app|
+      app.config.assets.precompile += %w( mxit_rails/emulator.js mxit_rails/emulator.css mxit_rails/included.css )
+    end
   end
 end
