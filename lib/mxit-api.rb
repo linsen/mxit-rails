bac5171aa964f1393e4e28fdeccc877914b35215
Rehan van Zyl <rehan@bazookagames.com>
2012-10-18 15:47:14 +0200

    Created a config for MxitApi.

--- lib/mxit-api.rb
+++ lib/mxit-api.rb
@@ -2,6 +2,22 @@ require "mxit_api/client"
 require "mxit_api/auth_token"
 require "mxit_api/exception"
 require "mxit_api/request_exception"
+require "mxit_api/config"
 
 module MxitApi
+  extend self
+  attr_accessor :config
+
+  # Call this method to modify defaults in your initializers.
+  #
+  # @example
+  #   MxitApi.configure do |config|
+  #     config.mxit_app_name = 'mxitmoney'
+  #   end
+  def configure
+    self.config ||= MxitApi::Config.new
+    yield(config)
+  end
 end
+
+MxitApi.configure {}
